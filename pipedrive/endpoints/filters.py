0881ce00272d805f5a90e90af3e16d55445a5c74
Albin Anthony (Ibha-X) <89918383+IbhaX@users.noreply.github.com>
2023-03-02 12:50:03 +0530

    updated string formatting

--- pipedrive/endpoints/filters.py
+++ pipedrive/endpoints/filters.py
@@ -3,7 +3,7 @@ class Filters(object):
         self._client = client
 
     def get_filter(self, filter_id, **kwargs):
-        url = "filters/{}".format(filter_id)
+        url = f"filters/{filter_id}"
         return self._client._get(self._client.BASE_URL + url, **kwargs)
 
     def get_all_filters(self, params=None, **kwargs):
@@ -15,9 +15,9 @@ class Filters(object):
         return self._client._post(self._client.BASE_URL + url, json=data, **kwargs)
 
     def update_filter(self, filter_id, data, **kwargs):
-        url = "filters/{}".format(filter_id)
+        url = f"filters/{filter_id}"
         return self._client._put(self._client.BASE_URL + url, json=data, **kwargs)
 
     def delete_filter(self, filter_id, **kwargs):
-        url = "filters/{}".format(filter_id)
+        url = f"filters/{filter_id}"
         return self._client._delete(self._client.BASE_URL + url, **kwargs)
